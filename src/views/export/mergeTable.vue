9e7d5d06b5388d6579e1f1c951a425015d116584
wilson <Yinxinxin@UNIVERSEACTION.com>
2024-07-17 14:30:42 +0800

    chore: 添加忽略文件

--- src/views/export/mergeTable.vue
+++ src/views/export/mergeTable.vue
@@ -1,65 +1,96 @@
 <template>
   <div>
-    <el-upload class="upload-demo" action="/" :http-request="(params) => handleRequest(params)">
-      <el-button type="primary" icon="el-icon-upload2">点击上传表格</el-button>
+    <el-upload class="upload-demo" :file-list="fileList" action="#" :auto-upload="false" :on-change="handleChange"
+      :on-remove="handleChange">
+      <el-button slot="trigger" size="small" type="primary" icon="el-icon-upload2">选取表格</el-button>
+      <el-button :loading="parseLoading" style="margin-left: 10px;" size="small" type="success" @click="btnParse">{{
+      parseLoading ? '解析中' : '解析并下载' }}</el-button>
+      <div slot="tip" v-if="showTip" class="el-upload__tip" style="color:red">请先上传文件~</div>
     </el-upload>
   </div>
 </template>
 
 <script>
 import xlsx from 'xlsx';
+import Papa from 'papaparse'
 import { parseTime } from '@/utils'
 
 export default {
   data() {
     return {
+      fileList: [],
       importData: [],
-      exportData: []
+      exportData: [],
+      showTip: false,
+      parseLoading: false,
+      lastIndex: 1,
+      timer: null
     }
   },
   methods: {
-    extractOrderNumber(str) {
-
+    handleChange(file, fileList) {
+      console.log(file, fileList);
+      this.fileList = fileList
+      if (this.fileList.length) this.showTip = false
     },
-    async handleRequest(params) {
-      const { file } = params
-      // 读取表格对象
-      const buffer = await file.arrayBuffer()
-      const workbook = xlsx.read(buffer, {
-        type: 'buffer',
-        cellDates: true,//设为true，将天数的时间戳转为时间格式
-      });
-      // 找到第一张表
-      const sheetNames = workbook.SheetNames;
-      const sheet1 = workbook.Sheets[sheetNames[0]];
-      // 读取内容
-      const jsonData = xlsx.utils.sheet_to_json(sheet1);
-      const index = jsonData.findIndex(item => item['#支付宝收支明细'] === '流水号') + 1
-      this.importData = jsonData.slice(index, jsonData.length - 1)
-      console.log(this.importData);
-      this.importData.forEach(item => {
-        if ('__EMPTY_2' in item) {
-          const str = item['__EMPTY_2']
-          const startIndex = str.indexOf('订单号:') + 4
-          const endIndex = str.lastIndexOf(")")
-          console.log(str.slice(startIndex, endIndex));
-        }
+    btnParse() {
+      if (!this.fileList.length) {
+        this.showTip = true
+        return
+      }
 
-
-        this.exportData.push({
-          '日期': parseTime(item['__EMPTY']),
-          // '订单号': str.slice(startIndex, endIndex)
+      this.parseLoading = true
+      this.importData = []
+      this.exportData = []
+      this.fileList.forEach(item => {
+        Papa.parse(item.raw, {
+          encoding: 'GB2312', // 编码格式
+          complete: ({ data }) => {
+            const index = data.findIndex(key => key[0] === '流水号') + 1
+            this.importData.push(...data.slice(index, data.length - 2))
+          }
         })
       })
-      console.log(this.exportData);
 
-      // this.exportExcelFile([{
-      //   "name": '里斯',
-      //   "age": '12'
-      // },{
-      //   "name": '张三',
-      //   'age': '999'
-      // }])
+      // 定时器判断文件是否解析完
+      this.timer = setInterval(() => {
+        if (this.firstIndex !== this.importData.length) {
+          this.firstIndex = this.importData.length
+        } else {
+          if (this.timer !== null) clearInterval(this.timer)
+          this.handleExportData()
+        }
+      }, 200);
+    },
+    extractNumberFromStart(str, startChar) {
+      const startIndex = str.indexOf(startChar) + 1;
+      const numStr = str.slice(startIndex).match(/\d+/);
+      return numStr ? numStr[0] : null;
+    },
+    extractStr(str, startChar) {
+      const startIndex = str.indexOf(startChar) - 1;
+      let sliceStr = str.slice(0, startIndex)
+      if(str.indexOf('订单号:')) sliceStr += ')'
+      return sliceStr
+    },
+    handleExportData() {
+      console.log(this.importData);
+      this.importData.forEach(item => {
+        const list = {
+          '日期': item[1],
+          '订单号': (item[3] && item[3].indexOf('订单号') !== -1) ? this.extractNumberFromStart(item[3], '订单号') : '',
+          '收入': item[4],
+          '支出': item[5],
+          '摘要': (item[3] && item[3].indexOf('订单号') !== -1) ? this.extractStr(item[3], '订单号') : item[3],
+          '支付方式': item[2],
+          '余额': item[6],
+          '资金渠道': item[7]
+        }
+
+        this.exportData.push(list)
+      })
+      this.exportExcelFile(this.exportData, 'table1', `example_${parseTime(new Date(), '{y}-{m}-{d}')}.xlsx`)
+      this.parseLoading = false
     },
     exportExcelFile(array, sheetName = '表1', fileName = 'example.xlsx') {
       const jsonWorkSheet = xlsx.utils.json_to_sheet(array);
